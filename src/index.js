84d78f583e90e30de70265ea05c8fe8cf3579c8b
Wild-bit <1397119446@qq,com>
2022-01-08 00:57:08 +0800

    增加babel配置，安装对应转为浏览器兼容的js语法依赖

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+let a = 1
+let b = 2
+let c = a+b
+
+// Promise是api层面的，是一个es6中的全局对象
+const p = new Promise((resolve, reject) => {    
+    resolve(100);
+  });
+
+const list = [1,2,3,4].map(item => item*2)
+
+class Student {
+    constructor(name,age){
+        this.name = name;
+        this.age = age
+    }
+}
