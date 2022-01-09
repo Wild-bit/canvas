f33f19fd1019d131f55fdf905af41fc4aa2ecaae
Wild-bit <1397119446@qq,com>
2022-01-10 00:30:01 +0800

    增加babel对react的presets，使babel能够支持编译jsx语法

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: "bundle.js"
     },
     devServer: {
         static: {
             directory: path.join(__dirname, 'public'),
+        },
+        client: {
+            overlay: true,
+            progress: true,
         },
         compress: true,
         hot: true, //热更新
@@ -21,7 +25,7 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,    // .ts或者tsx后缀的文件，就是typescript文件
-                use: "ts-loader",   // 就是上面安装的ts-loader
+                use: "ts-loader",   // 通过 ts-loader 加载所有的 .ts 和 .tsx 文件
                 exclude: "/node-modules/" // 排除node-modules目录
             },
             {
@@ -36,11 +40,14 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'canvas'
+            title: 'canvas',
+            filename: 'index.html',
+            template: 'public/index.html',
+            inject: true
         })
     ],
     resolve: {
-        extensions: ['.ts', '.js'],
+        extensions: ['.ts', '.js', '.tsx'],
         alias: {
             "COMMON": path.resolve(__dirname, 'common/')
         },
